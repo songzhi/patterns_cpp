8d4a6aea1d8e7fc4bbbcb4dc6f18ab896344ac08
lsongzhi <lsongzhi@163.com>
2020-05-06 11:23:16 +0800

    feat: factory method

---
+++ src/FactoryMethod/main.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+
+class Button {
+public:
+    virtual void render() const = 0;
+
+    virtual void onClick(void (*f)()) = 0;
+};
+
+class WindowsButton : public Button {
+    void render() const override {
+        std::cout << "It a Windows Button";
+    }
+
+    void onClick(void (*f)()) override {}
+};
+
+class HTMLButton : public Button {
+    void render() const override {
+        std::cout << "It a HTML Button";
+    }
+
+    void onClick(void (*f)()) override {}
+};
+
+void closeDialog() {}
+
+class Dialog {
+public:
+    [[nodiscard]] virtual Button *createButton() const = 0;
+
+    void render() const {
+        auto okButton = createButton();
+        okButton->onClick(closeDialog);
+        okButton->render();
+    }
+};
+
+class WindowsDialog : public Dialog {
+    [[nodiscard]] Button *createButton() const override {
+        return new WindowsButton();
+    }
+};
+
+class WebDialog : public Dialog {
+    [[nodiscard]] Button *createButton() const override {
+        return new HTMLButton();
+    }
+};
+
+class Application {
+public:
+    Dialog *dialog;
+
+    explicit Application(const std::string &os) {
+        if (os == "Windows") {
+            dialog = new WindowsDialog();
+        } else if (os == "Web") {
+            dialog = new WebDialog();
+        }
+    }
+
+    void main() const {
+        dialog->render();
+    }
+};
+
+int main() {
+    auto app = Application("Windows");
+    app.main();
+}
